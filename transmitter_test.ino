0cbb528fc0bd7dd8051cf3b1afb79925c402e26d
Keanu-n <keanu.lennep@gmail.com>
2021-12-18 13:49:20 +0100

    working transmitter code

--- transmitter_test.ino
+++ transmitter_test.ino
@@ -2,19 +2,25 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(5, 6); // CE, CSN (9, 10)
+RF24 radio(4, 5); // CE, CSN (9, 10)
 const byte address[6] = "00001";
 
-String s = "This is working";
+int s = 12;
 
 void setup() {
+  Serial.begin(9600);
+  
   radio.begin();                  //Starting the Wireless communication
   radio.openWritingPipe(address); //Setting the address where we will send the data
   radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.setChannel(115);
   radio.stopListening();
 }
 
 void loop() {
-  radio.write(&s, sizeof(s));
-  delay(200);
+  if(!radio.write(&s, sizeof(s))){
+    Serial.println("It didn't work");
+  }else{
+    Serial.println("IT DID WORK");
+  }
 }

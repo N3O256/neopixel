fd74a19d78725215e8137fa0e943018bbbf22a0c
N3O256 <90545005+N3O256@users.noreply.github.com>
2022-02-19 19:34:30 +0100

    new version (hopefully better for remote)

---
+++ remote-v2.ino
@@ -0,0 +1,114 @@
+#include <RF24.h>
+
+RF24 radio(9, 10); // CE, CSN (9, 10)
+const byte address[6] = "00001";
+
+int xAxis1 = 0;
+int yAxis1 = 0;
+boolean buttonAxis = false;
+boolean buttonAxisLast = false;
+
+boolean button1 = false;
+boolean button1Last = false;
+boolean button2 = false;
+boolean button2Last = false;
+boolean button3 = false;
+boolean button3Last = false;
+
+int receive = 0;
+int transmit = 0;
+
+void setup() {
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.setChannel(115);
+  radio.stopListening();
+}
+
+void loop() {
+  readValues();
+  transmit = getChanges();
+  radio.write(&transmit, sizeof(transmit));
+}
+
+void readValues(){
+  button1 = digitalRead(2);
+  button2 = digitalRead(3);
+  button3 = digitalRead(4);
+  buttonAxis = digitalRead(7);
+}
+
+int getChanges(){
+  if(button1 != button1Last){
+    receive = 1;
+    button1Last = button1;
+  }else{
+    receive = 0;
+  }
+
+  if(button2 != button2Last){
+    receive = 2;
+    button2Last = button2;
+  }else{
+    receive = 0;
+  }
+
+  if(button3 != button3Last){
+    receive = 3;
+    button3Last = button3;
+  }else{
+    receive = 0;
+  }
+
+  if(buttonAxis != buttonAxisLast){
+    receive = getIntValueOfCustom();
+    buttonAxisLast = buttonAxis;
+  }else{
+    receive = 0;
+  }
+
+  return receive;
+}
+
+int getIntValueOfCustom(){
+  int number = 0;
+  
+  xAxis1 = analogRead(A2);
+  map(xAxis1, 0, 1023, -18, 18);
+  
+  yAxis1 = analogRead(A3);
+  map(yAxis1, 0, 1023, -18, 18);
+  
+  if(yAxis1 == 0 && xAxis1 == 0){
+    number = 4;
+  }else if(yAxis1 < 6 && xAxis1 > 0){
+    number = 5;
+  }else if(yAxis1 > 6 && yAxis1 < 12 && xAxis1 > 0){
+    number = 6;
+  }else if(yAxis1 >= 12 && xAxis1 > 0){
+    number = 7;
+  }else if(yAxis1 >= 12 && xAxis1 < 0){
+    number = 8;
+  }else if(yAxis1 > 6 && yAxis1 < 12 && xAxis1 < 0){
+    number = 9;
+  }else if(yAxis1 < 6 && xAxis1 > 0){
+    number = 10;
+  }
+
+  else if(yAxis1 < 6 && xAxis1 > 0){
+    number = 11;
+  }else if(yAxis1 > 6 && yAxis1 < 12 && xAxis1 > 0){
+    number = 12;
+  }else if(yAxis1 >= 12 && xAxis1 > 0){
+    number = 13;
+  }else if(yAxis1 >= 12 && xAxis1 < 0){
+    number = 14;
+  }else if(yAxis1 > 6 && yAxis1 < 12 && xAxis1 < 0){
+    number = 15;
+  }else if(yAxis1 < 6 && xAxis1 > 0){
+    number = 16;
+  }
+
+  return number;
+}
